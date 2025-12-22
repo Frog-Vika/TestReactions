d42ed864bba397430e61d325a4117791acd7f31c
Frog_Vika <viksagrigoreva@gmail.com>
2025-12-22 22:20:09 +0700

    Initial commit

--- app/src/main/java/com/example/testreactions/PlayFragment.kt
+++ app/src/main/java/com/example/testreactions/PlayFragment.kt
@@ -2,6 +2,8 @@ package com.example.testreactions
 
 import android.os.Bundle
 import android.os.SystemClock
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,7 @@ import com.example.testreactions.databinding.FragmentPlayBinding
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import kotlin.random.Random
+//import java.util.logging.Handler
 
 private lateinit var viewModel: LoginViewModel
 
@@ -21,7 +24,11 @@ class PlayFragment : Fragment(R.layout.fragment_play) {
     private val binding get() = _binding!!
 
     private var startTime = 0L
-    private var isRunning = false
+    private var signalStarted = false
+    private var waitingForSignal = false
+
+    //private val handler = Handler(Looper.getMainLooper())
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,7 +42,7 @@ class PlayFragment : Fragment(R.layout.fragment_play) {
     private val handler = android.os.Handler()
     private val updateRunnable = object : Runnable {
         override fun run() {
-            if (isRunning) {
+            if (signalStarted) {
                 val current = SystemClock.elapsedRealtime() - startTime
                 binding.Text.text = "Прошло: ${current} мс"
                 handler.postDelayed(this, 16) // обновление 60 раз в секунду
@@ -61,39 +68,92 @@ class PlayFragment : Fragment(R.layout.fragment_play) {
         }
 
         binding.root.setOnClickListener {
-            if (!isRunning) {
-                // Первый клик → стартуем таймер
-                startTime = SystemClock.elapsedRealtime()
-                isRunning = true
-
-                //binding.button2.text = "Stop"
-                //binding.Text.text = "Время идёт"
-                binding.Text.text = "Прошло: 0 мс"
-
-                handler.post(updateRunnable)
-
-
-                /*Toast.makeText(requireContext(), "Таймер запущен!", Toast.LENGTH_SHORT).show()*/
-
-            } else {
-                // Второй клик → стоп
-                val endTime = SystemClock.elapsedRealtime()
-                val finalTime = endTime - startTime // разница в миллисекундах
-
-                binding.Text.text = "Итоговое время: ${finalTime} мс"
-                if ((viewModel.time.value > finalTime) or (viewModel.time.value == 0L)) {viewModel.time.value = finalTime}
-                //viewModel.time.value = finalTime
-                //binding.button2.text = "Start"
-
-                /*Toast.makeText(
-                    requireContext(),
-                    "Прошло: ${diff} мс",
-                    Toast.LENGTH_LONG
-                ).show()*/
-
-                isRunning = false
+            when {
+                !waitingForSignal && ! signalStarted -> {
+                   startWaitingForSignal()
+                }
+                waitingForSignal && ! signalStarted -> {
+                    errorClick()
+                }
+                signalStarted -> {
+                    stopTimer()
+                }
             }
         }
+    }
+
+    private fun startWaitingForSignal() {
+        waitingForSignal = true
+        signalStarted = false
+
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.white)
+        )
+
+        binding.Text.text = "Ждте сигнала ..."
+
+        val delay = Random.nextLong(1000, 5000)
+
+        handler.postDelayed({showSignal()}, delay)
+    }
+
+    private fun showSignal() {
+        waitingForSignal = false
+        signalStarted = true
+
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.signal_color)
+        )
+
+        binding.Text.text = "ЖМИ!!!"
+
+        /*handler.postDelayed({
+            binding.root.setBackgroundColor(
+                requireContext().getColor(R.color.white)
+            )
+        }, 100)*/
+
+
+        startTime = SystemClock.elapsedRealtime()
+        handler.post(updateRunnable)
+    }
+
+    private fun errorClick() {
+        waitingForSignal = false
+        //signalStarted = false
+
+        handler.removeCallbacksAndMessages(null)
+
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.error_color)
+        )
+        binding.Text.text = "Слишком рано"
+
+        startTime = SystemClock.elapsedRealtime()
+
+        handler.postDelayed({resetGame()}, 1000)
+    }
+
+    private fun resetGame() {
+        waitingForSignal = false
+        signalStarted = false
+
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.white)
+        )
+
+        binding.Text.text = "Нажми, чтобы начать"
+    }
+    private fun stopTimer() {
+        signalStarted = false
+        //handler.removeCallbacks(updateTimerRunnable)
+
+        val finalTime = SystemClock.elapsedRealtime() - startTime
+        if ((viewModel.time.value > finalTime) or (viewModel.time.value == 0L)) {viewModel.time.value = finalTime}
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.white)
+        )
+        binding.Text.text = "Результат: ${finalTime} мс\nНажми снова чтобы начать"
     }
 
     override fun onDestroyView() {
